fa4dc36ee04eef9386dc5f6ac021529871ef3ac0
reza48449-hitam <rayy48449@gmail.com>
2026-08-30 13:41:18 +0800

    Delete modules directory

--- modules/routes.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-function init(app) {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-    });
-}
-
-module.exports = { init };
