59e548aebeaeec5aee1b2a3b2a498c3dd3999448
reza48449-hitam <rayy48449@gmail.com>
2026-09-02 00:08:48 +0800

    Update gamevar.js

--- gamevar.js
+++ gamevar.js
@@ -94,7 +94,7 @@ const gamevarLines = [
   "FreeMoveAngularSpeedCreep,FreeMoveAngularSpeedCreep,float,100000,,",
   "ResetRotationSpeed,ResetRotationSpeed,float,100000,,",
   "RunSpeed,RunSpeed,float,5.5,,",
-  "DashSpeedScale,DashSpeedScale,float,10.5,,",
+  "DashSpeedScale,DashSpeedScale,float,6.0,,",
   "CrouchSpeed,CrouchSpeed,float,4.5,,",
 ];
 
